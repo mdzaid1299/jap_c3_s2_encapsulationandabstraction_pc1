9043c522dbab2aec5c9a6674d978ab8a7a27b590
bhakti.rathod <bhakti.rathod@stackroute.in>
2021-09-09 12:02:02 +0530

    Initial commit

---
+++ src/main/java/com/jap/bookstore/Author.java
@@ -0,0 +1,12 @@
+package com.jap.bookstore;
+
+public class Author {
+    String authorName;
+    String authorPenName;
+     // Declare a parameterized constructor to initialize values
+    public Author(String authorName, String authorPenName) {
+
+    }
+
+
+}
---
+++ src/main/java/com/jap/bookstore/Bookstore.java
@@ -0,0 +1,16 @@
+package com.jap.bookstore;
+
+public class Bookstore {
+    String bookStoreName;
+    String bookStoreRegistrationId;
+    Book[] books;
+    // Declare a parameterized constructor to initialize values
+    public Bookstore(String bookStoreName, String bookStoreRegistrationId, Book[] books) {
+
+    }
+    // Function to calculate the discount for a genre of books
+    public float calculateDiscount(Book book,float discountPercentage)
+    {
+        return -1;
+    }
+}
