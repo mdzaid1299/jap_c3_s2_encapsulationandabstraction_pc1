84a2ba7a5f2ab9295180dab10246d576e905ff1e
Mohd Zaid <105915677+mdzaid1299@users.noreply.github.com>
2023-05-04 23:56:46 +0530

    ini

--- src/main/java/com/jap/bookstore/BookStoreImpl.java
+++ src/main/java/com/jap/bookstore/BookStoreImpl.java
@@ -3,15 +3,17 @@ package com.jap.bookstore;
 public class BookStoreImpl {
     public static void main(String[] args) {
         // Declare and initialize author, book and bookstore objects
-
+            Author author = new Author();
+            Bookstore bookStore = new bookstore();
+            Book book = new Book();
         //Display book details before discount is calculated
         System.out.println("The book details before discount : ");
-
+        System.out.println(book.displayBookDetails())
         //Calculate discount on the books by calling the methods
-
+        bookstore.calculateDiscount()
         //Display book details after discount is calculated
         System.out.println("The book details after discount : ");
-
+        
         }
 
 }
